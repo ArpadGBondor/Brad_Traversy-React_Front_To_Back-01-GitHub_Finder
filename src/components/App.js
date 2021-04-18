05dfd50c215f361ba88c61baaf0f9511ce6ce0fb
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2021-04-18 09:45:48 +0100

    test netlify

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,7 @@ class App extends Component {
     this.setState({ loading: true });
 
     try {
-      const res = await axios('/.netlify/functions/github');
+      const res = await axios('/.netlify/functions/helloWorld');
 
       // let res = await axios('https://api.github.com/users', {
       //   headers: {
