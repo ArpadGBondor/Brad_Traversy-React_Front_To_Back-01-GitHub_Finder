fdecea3537b0052a594715470f7196d15389fde7
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2021-04-18 10:57:25 +0100

    test netlify

--- .netlify/functions/github.js
+++ .netlify/functions/github.js
@@ -1,19 +1,38 @@
-const axios = require('axios');
+const API_ENDPOINT = 'https://api.github.com/users'
 
-exports.handler = async function (event, context) {
+exports.handler = async (event, context) => {
+  let response
   try {
-    let response = await axios('https://api.github.com/users');
-
-    let data = response;
-    return { statusCode: 200, body: data };
+    response = await fetch(API_ENDPOINT)
+    // handle response
+    return { statusCode: 200, body: response };
   } catch (err) {
     return {
-      statusCode: 200,
+      statusCode: err.statusCode || 500,
       body: JSON.stringify({
-        error: err,
-        statusCode: err.statusCode || 500,
-        message: err.message,
-      }),
-    };
+        error: err.message
+      })
+    }
   }
-};
+
+
+
+// const axios = require('axios');
+
+// exports.handler = async function (event, context) {
+//   try {
+//     let response = await axios('https://api.github.com/users');
+
+//     let data = response;
+//     return { statusCode: 200, body: data };
+//   } catch (err) {
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify({
+//         error: err,
+//         statusCode: err.statusCode || 500,
+//         message: err.message,
+//       }),
+//     };
+//   }
+// };
