c1cfeff168a2be43433cc3e61da7b27281b65797
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2021-04-18 08:38:34 +0100

    test netlify

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ class App extends Component {
 
     const res = await fetch('../../.netlify/functions/github');
 
-    console.log(res);
+    console.log(res.body.json());
 
     this.setState({
       users: res.data,
