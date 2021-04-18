88138cf76828c59a4831f73e740e4400a56e56d6
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2021-04-18 10:08:05 +0100

    test netlify

--- .netlify/functions/github.js
+++ .netlify/functions/github.js
@@ -1,4 +1,4 @@
-// import axios from 'axios'
+import axios from 'axios';
 
 exports.handler = async function (event, context) {
   return { statusCode: 200, body: JSON.stringify({ message: process.env.SECRET }) };
