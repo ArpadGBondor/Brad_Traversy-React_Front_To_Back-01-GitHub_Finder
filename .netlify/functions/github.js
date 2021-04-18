40e67215248b024cb935b72e17e2fa58cced5096
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2021-04-18 09:52:31 +0100

    test netlify

--- .netlify/functions/github.js
+++ .netlify/functions/github.js
@@ -1,22 +1,24 @@
-import axios from 'axios';
-
 exports.handler = async function (event, context) {
-  return { statusCode: 200, body: JSON.stringify({ message: process.env.SECRET }) };
-
-  try {
-    let response = await axios('https://api.github.com/users', {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_PUBLIC_ACCESS_TOKEN}`,
-      },
-    });
-
-    let data = response;
-    return { statusCode: 200, body: JSON.stringify(data) };
-  } catch (err) {
-    let error = {
-      statusCode: err.statusCode || 500,
-      body: JSON.stringify({ error: err.message }),
-    };
-    return error;
-  }
+  return { statusCode: 200, body: JSON.stringify({ message: 'Hello World' }) };
 };
+
+// exports.handler = async function (event, context) {
+//   return { statusCode: 200, body: JSON.stringify({ message: process.env.SECRET }) };
+
+//   try {
+//     let response = await axios('https://api.github.com/users', {
+//       headers: {
+//         Authorization: `token ${process.env.REACT_APP_GITHUB_PUBLIC_ACCESS_TOKEN}`,
+//       },
+//     });
+
+//     let data = response;
+//     return { statusCode: 200, body: JSON.stringify(data) };
+//   } catch (err) {
+//     let error = {
+//       statusCode: err.statusCode || 500,
+//       body: JSON.stringify({ error: err.message }),
+//     };
+//     return error;
+//   }
+// };
