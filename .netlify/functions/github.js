221f1135bfe1a86adf493faf8e0db710de1f2a4d
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2021-04-18 08:46:04 +0100

    test netlify

--- .netlify/functions/github.js
+++ .netlify/functions/github.js
@@ -1,19 +1,18 @@
+import axios from 'axios';
+
 exports.handler = async (event, context, callback) => {
   const pass = (body) => {
     callback(null, { statusCode: 200, body: JSON.stringify(body) });
   };
 
   try {
-    let response = await fetch('https://api.github.com/users', {
-      method: event.httpMethod,
+    let response = await axios('https://api.github.com/users', {
       headers: {
         Authorization: `token ${process.env.REACT_APP_GITHUB_PUBLIC_ACCESS_TOKEN}`,
-        'Content-Type': 'application/json',
       },
-      body: event.body,
     });
 
-    let data = await response.json();
+    let data = response.data;
     await pass(data);
   } catch (err) {
     let error = {
